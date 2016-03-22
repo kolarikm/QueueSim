b5b5ddbf2c40505dcaa40254e024933799936ba3
kolarikm <kolarikm@mail.gvsu.edu>
2016-03-22 00:27:05 -0400

    Now reads the dat into an array

--- proj2.c
+++ proj2.c
@@ -13,24 +13,28 @@ double expdist (double mean)
     return -mean * log(r);
 }
 
-void split_str(char *s)
+void split_str(char *s, int *r)
 {
     char * token1;
     char * token2;
-    token1 = strtok(s, "\t");
-    token2 = strtok(NULL, "\n");
-    printf("%s, %s\n", token1, token2);
+    //token1 = strtok(s, "\t");
+    //token2 = strtok(NULL, "\n");
+    r[0] = atoi(strtok(s, "\t"));
+    r[1] = atoi(strtok(NULL, "\n"));
 }
 
 void read_data()
 {
     FILE *in;
     char s[20];
-    char *sp;
+    int r[5][2];
+    int i = 0;
     in = fopen("proj2.dat", "r");
     while (fgets(s, 20, in) != NULL)
     {
-        split_str(s);
+        split_str(s, r[i]);
+        printf("%d   %d\n", r[i][0], r[i][1]);
+        i++;
     }
 }
 
@@ -42,12 +46,12 @@ void simulation (int numOfTellers)
     //Somehow need to simulate 480 separate minutes
     // !!!  Do this first --> Each minute, calculate random 1-100 number to get number of arriving customers using proj2.dat
             // DONE!  Read in data
-            // place into 2d array
+            // DONE!  Place into 2d array
             // Write function that takes in a int 1-100 and determines how many customers will be served that minute by looking at number in col2 and then getting and returning col1
     //Use expdist to figure out how long serving that customer takes for each teller
     //One queue with 1-100 new elems getting added every 60 ticks
     //Each teller can handle 60 / that teller's expdist number of customers per tick
-    //   
+    //enqueue(cust_id, a_time, &q); cust_id++ after every enqueue   
 }
 
 int main ()
